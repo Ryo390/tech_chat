74537fdf62196949c32eb34868198bcd0c580f1f
Ryo390 <151626117+Ryo390@users.noreply.github.com>
2024-01-26 14:43:28 +0900

    質問投稿機能の実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,20 @@
 class ChatsController < ApplicationController
 
   def index
+    @chats = Chat.all
   end
 
+  def new
+    @chat = Chat.new
+  end
+
+  def create
+    Chat.create(chat_params)
+    redirect_to root_path
+  end
+
+  private
+  def chat_params
+    params.require(:chat).permit(:title, :content, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: "chats#index"
+  get "/index", to: "chats#index", as: :index
+  get "/new", to: "chats#new", as: :new_chat
+  post "/new", to: "chats#create"
 end
+
